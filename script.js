10c807956184bf5e9511b6f644cb0fe9f0e69781
mohrcookies <07_road_bowl@icloud.com>
2025-01-28 12:49:04 -0500

    Create customSize function
    This function effectively creates a grid based on
    number input. It sets height/width attribute to grid
    divs based on dimensions of container

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
 
-let j = 16;
+//function to create grid
 function createGrid() {
-    for (let i = 1; i <= j; i++) {
+    for (let i = 1; i <= 1000; i++) {
         let divGrid = document.createElement('div');
-        divGrid.classList.add(`div${i}`);
+        divGrid.classList.add('grid');
         container.appendChild(divGrid);
     }
 }
 
-createGrid();
+//this calls the function and actually creates the grid
+// createGrid();
+
+
+//function for custom user size
+function customSize(size) {
+    gridSize = size * size;
+    for (let i = 1; i <= gridSize; i++) {
+        let divGrid = document.createElement('div');
+        divGrid.classList.add('grid');
+        divGrid.setAttribute('style', `height: ${600/size}px; width: ${600/size}px`);
+        container.appendChild(divGrid);
+    }
+}
+
+customSize(16);
