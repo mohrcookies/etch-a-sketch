17b610c1e94a8675ca84f9245b2ad3d9a96d224f
mohrcookies <07_road_bowl@icloud.com>
2025-01-29 13:32:11 -0500

    Add user-input button. Add userInput function

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
 //references
 const container = document.querySelector('.container');
+const userInput = document.querySelector('.user-input');
 
 //function to create grid
-function createGrid() {
-    for (let i = 1; i <= 1000; i++) {
+createDefaultGrid(16);
+function createDefaultGrid() {
+    for (let i = 1; i <= 256; i++) {
         let divGrid = document.createElement('div');
         divGrid.classList.add('grid');
+        divGrid.setAttribute('style', `height: ${600/16}px; width: ${600/16}px`);
+        divGrid.addEventListener('mouseover', (event) => { 
+            event.target.style.backgroundColor = "purple";
+            });
         container.appendChild(divGrid);
     }
 }
 
-//this calls the function and actually creates the grid
-// createGrid();
-
-
 //function for custom user size
-function customSize(size) {
+function customGrid(size) {
     gridSize = size * size;
     for (let i = 1; i <= gridSize; i++) {
         let divGrid = document.createElement('div');
         divGrid.classList.add('grid');
         divGrid.setAttribute('style', `height: ${600/size}px; width: ${600/size}px`);
+        divGrid.addEventListener('mouseover', (event) => { 
+            event.target.style.backgroundColor = "purple";
+            });
         container.appendChild(divGrid);
     }
 }
 
-customSize(16);
 
-//hover effect
-const grid = document.querySelectorAll('.grid');
-grid.forEach((square) => {
-    square.addEventListener('mouseover', (event) => {   //this is an anonymous function w/ parameter 'event'
-    event.target.style.backgroundColor = "purple";
-    });
-});
-
+//button to prompt for new grid dimensions
+let userSize;
+userInput.addEventListener('click', () => {
+    userSize = prompt('Pick a grid width between 1 and 100.');
+    parseInt(userSize);
+    customGrid(userSize);
+})
 
 
 
@@ -43,27 +46,30 @@ grid.forEach((square) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-// //function for custom user size
-// customSize(16);
-// function customSize(size) {
+//alternative function for custom user size
+// function customGrid(size) {
 //     gridSize = size * size;
 //     for (let i = 1; i <= gridSize; i++) {
 //         let divGrid = document.createElement('div');
 //         divGrid.classList.add('grid');
 //         divGrid.setAttribute('style', `height: ${600/size}px; width: ${600/size}px`);
-//         divGrid.addEventListener('mouseover', (event) => { 
-//             event.target.style.backgroundColor = "purple";
-//             });
 //         container.appendChild(divGrid);
 //     }
 // }
+//hover effect
+// const grid = document.querySelectorAll('.grid');
+// grid.forEach((square) => {
+//     square.addEventListener('mouseover', (event) => { 
+//     event.target.style.backgroundColor = "purple";
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
